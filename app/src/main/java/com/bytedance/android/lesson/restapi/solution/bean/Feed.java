c6d6f4789b0dc4c49692e69bb65ff794eefb976b
hdl794613 <243662291@qq.com>
2019-07-16 17:13:02 +0800

    Initial commit

---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/Feed.java
@@ -0,0 +1,21 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.20 14:18
+ */
+public class Feed {
+    @SerializedName("student_id") private String student_id;
+    @SerializedName("user_name") private  String user_name;
+    @SerializedName("image_url") private  String image_url;
+    @SerializedName("_id") private String _id;
+    @SerializedName("video_url") private String video_url;
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    // TODO-C2 (1) Implement your Feed Bean here according to the response json
+}
---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/PostVideoResponse.java
@@ -0,0 +1,20 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.18 17:53
+ */
+public class PostVideoResponse {
+    private Object result;
+    private String url;
+    private boolean success;
+
+    public Object getResult() {
+        return result;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    // TODO-C2 (3) Implement your PostVideoResponse Bean here according to the response json
+}
